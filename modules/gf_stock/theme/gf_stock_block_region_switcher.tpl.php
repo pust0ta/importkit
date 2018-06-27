3ad868460534c3ab2b03fc8a9ecfedf67cc43b2e
dalay <dalay@drupalka.ru>
2018-06-28 00:45:04 +0300

    gf_stock: блок с кнопками для выбора региона

--- modules/gf_stock/theme/gf_stock_block_region_switcher.tpl.php
+++ modules/gf_stock/theme/gf_stock_block_region_switcher.tpl.php
@@ -1,7 +1,8 @@
 <?php if ($regions): ?>
 <ul>
-<?php foreach($regions as $region): ?>
-  <li><?php print $region; ?></li>
+<?php foreach($regions as $region_name => $region_link): ?>
+  <li<?php if ($current_region == $region_name) 
+              print ' class="active"'; ?>><?php print $region_link; ?></li>
 <?php endforeach; ?>
 </ul>
 <?php endif; ?>
